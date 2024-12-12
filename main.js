f6a42c84b023bc143e6ac4f80e267dda25bb0938
nkhaccuong <cuong19012002@gmail.com>
2024-12-12 22:56:46 +0700

    import cors URLFE

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const dotenv = require('dotenv');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const authRoutes = require('./src/user/routes/authRoutes');
-const productRoutes = require('./src/products/routes/productRoutes'); 
+const productRoutes = require('./src/products/routes/productRoutes');
 const supplierRouter = require('./src/supplier/routes/supplierRouter');
 const warehouseRouter = require('./src/warehouse/routes/warehouseRouter');
 const cartRouter = require('./src/cart/routes/cartRouter');
@@ -16,10 +16,10 @@ const promotionProgramRoutes = require("./src/promotion/router/promotionProgramR
 const voucherRoutes = require("./src/promotion/router/voucherRoutes");
 
 const stockRouter = require('./src/warehouse/routes/StockRouter');
-const employeeRouter = require('./src/employee/routers/employeeRouter');  
+const employeeRouter = require('./src/employee/routers/employeeRouter');
 const transactionRoutes = require('./src/warehouse/routes/TransactionRoutes');
 const customerRoutes = require('./src/customer/routers/customerRoutes');
-const statisticsRouter = require('./src/bill/routes/statisticsRouter');  
+const statisticsRouter = require('./src/bill/routes/statisticsRouter');
 const returnbillRouter = require('./src/bill/routes/billReturn');
 const zalopayRouter = require('./src/zalopay/router/zalopayRouter')
 const vnpayRouter = require('./src/bill/routes/vnpayRouter');
@@ -31,7 +31,7 @@ app.use(express.json());
 app.use(cookieParser());
 // Cấu hình CORS
 app.use(cors({
-  origin: ['http://localhost:3000','http://192.168.1.9:8081'],
+  origin: ['http://localhost:3000', 'http://192.168.1.9:8081', 'https://c-mart-shop-fe.vercel.app'],
   credentials: true
 }));
 // app.use(cors()); 
@@ -44,21 +44,21 @@ app.use('/api/products', productRoutes);
 
 app.use('/api/suppliers', supplierRouter);
 app.use('/api/warehouses', warehouseRouter);
-app.use('/api/cart',cartRouter )
-app.use('/api/bill',billRouter )
+app.use('/api/cart', cartRouter)
+app.use('/api/bill', billRouter)
 app.use('/api/categories', categoryRoutes);
 
-app.use('/api/price-list',priceListRoutes)
+app.use('/api/price-list', priceListRoutes)
 app.use("/api/promotion-program", promotionProgramRoutes);
 app.use("/api/voucher", voucherRoutes);
 app.use("/api/stock", stockRouter);
 app.use("/api/employees", employeeRouter);
 app.use("/api/transactions", transactionRoutes);
 app.use("/api/customers", customerRoutes);
-app.use('/api/statistics',statisticsRouter )
-app.use('/api/return',returnbillRouter )
-app.use('/api/zalopay',zalopayRouter)
-app.use('/api/vnpay',vnpayRouter )
+app.use('/api/statistics', statisticsRouter)
+app.use('/api/return', returnbillRouter)
+app.use('/api/zalopay', zalopayRouter)
+app.use('/api/vnpay', vnpayRouter)
 // Kết nối MongoDB
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
